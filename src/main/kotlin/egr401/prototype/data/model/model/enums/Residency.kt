9bb82adca10a23aad5d065903c6306e2c7f3e9e9
TrevorRag <tdragland5@gmail.com>
2019-01-29 12:59:25 -0800

    Created User, Voter and Candidate Entities allowed for some Voter controlls with api

---
+++ src/main/kotlin/egr401/prototype/data/model/model/enums/Residency.kt
@@ -0,0 +1,6 @@
+package egr401.prototype.data.model.model.enums
+
+enum class Residency(val text: String) {
+    ON_CAMPUS("On-Campus"),
+    COMMUTER("Commuter")
+}
---
+++ src/main/kotlin/egr401/prototype/impl/persistence/daos/CandidateDao.kt
@@ -0,0 +1,32 @@
+package egr401.prototype.impl.persistence.daos
+
+import egr401.prototype.data.model.Candidate
+import egr401.prototype.inter.persistence.daos.Dao
+import org.springframework.stereotype.Repository
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@Repository
+class CandidateDao : Dao<Candidate> {
+
+    @PersistenceContext
+    private lateinit var entityManager: EntityManager
+
+    override fun insert(obj: Candidate) {
+        entityManager.persist(obj)
+    }
+
+    override fun getById(id: Int): Candidate {
+        return entityManager.find(Candidate::class.java, id)
+    }
+
+    override fun update(obj: Candidate): Candidate {
+        return entityManager.merge(obj)
+    }
+
+    override fun delete(obj: Candidate) {
+        entityManager.remove(obj)
+    }
+
+
+}
